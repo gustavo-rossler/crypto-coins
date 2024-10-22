a0bf780aa83cee0db5d06dc8bafdf8f35fb88b9c
Gustavo Pereira Rössler <gustavo_rossler@hotmail.com>
2024-10-22 00:35:58 -0300

    add use cases to service provider

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Repositories\EloquentCoinsRepository;
 use App\Repositories\ICoinsRepository;
 use Illuminate\Support\ServiceProvider;
+use App\UseCases\GetAllCryptoCoinsUseCase;
+use App\UseCases\GetCryptoCoinBySymbolUseCase;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(ICoinsRepository::class, EloquentCoinsRepository::class);
+        $this->app->bind(GetAllCryptoCoinsUseCase::class, GetAllCryptoCoinsUseCase::class);
+        $this->app->bind(GetCryptoCoinBySymbolUseCase::class, GetCryptoCoinBySymbolUseCase::class);
     }
 }
